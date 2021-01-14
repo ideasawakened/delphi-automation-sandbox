89e7671a9bfebd325d8472fa86da470df54ae903
Darian Miller <darian@darianmiller.com>
2021-01-13 19:48:48 -0600

    Customize path on dedicated build server

--- Utils/Build_10.4.bat
+++ Utils/Build_10.4.bat
@@ -1,6 +1,6 @@
 rem Verify and change to the path for your rsvars.bat as needed
 rem The default for 10.4 Sydney is C:\Program Files (x86)\Embarcadero\Studio\21.0\bin\rsvars.bat
-call "C:\Program Files (x86)\Embarcadero\Studio\21.0\bin\rsvars.bat"
+call "C:\Delphi10.4\bin\rsvars.bat"
 
 rem The first expected parameter is the target work folder
 cd /D %1
